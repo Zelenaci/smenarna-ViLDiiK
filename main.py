55f2667b9e8dd8f0c4e59a3a1764408e28ff5e1e
ViLDiiK <vilim.ondrej@gmail.com>
2022-03-29 15:12:10 +0200

    Done

--- main.py
+++ main.py
@@ -1,92 +1,98 @@
-from os.path import basename, splitext
-import tkinter as tk
 from tkinter import *
 
 
-class About(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent, class_=parent.name)
-        self.config()
+def kurzy():
+    f=open('listek.txt','r')
+    meny=[]
+    while 1:
+        radek = f.readline()
+        if radek == '':
+            break
+        if radek.strip()[0] == '#':
+            continue
+        meny += [ radek.replace(',','.').split() ]
+    f.close()
+    for radek in meny:
+        radek[1]=float(radek[1])
+        radek[2]=float(radek[2])
+        radek[3]=float(radek[3])
+    return meny
+mnozstvi = 1
+kurz = 1
+def prevody(*args):
+    global mnozstvi
+    global kurz
+    mnozstviVar.set(str(meny[menaVar.get()][1]))
+    count=meny[menaVar.get()][1]
+    if transakceVar.get()==0:
+        kurzVar.set(str(meny[menaVar.get()][2]))
+        kurz=meny[menaVar.get()][2]
+    else:
+        kurzVar.set(str(meny[menaVar.get()][3]))
+        kurz=meny[menaVar.get()][3]
 
-        btn = tk.Button(self, text="Konec", command=self.close)
-        btn.pack()
+def prepocet():
+    global mnozstvi
+    global kurz
+    vstup =  float(vstupVar.get())
+    vystup= kurz/mnozstvi * vstup
+    vystupVar.set(str(vystup))
 
-    def close(self):
-        self.destroy()
-
-
-class Application(tk.Tk):
-    name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Směnárna"
-
-    def __init__(self):
-        super().__init__(className=self.name)
-        self.v1 = tk.IntVar(self)
-        self.v2 = tk.IntVar(self)
-        self.title(self.name)
-        self.lbl = tk.Label(self, text="Směnárna", borderwidth=14)
-        self.lbl.pack()
-        
-        listek=open("listek.txt", "r")
-
-        self.lbl1 = tk.Label(self, text="Transakce:")
-        self.lbl1.pack(anchor=W)
-        self.radiobutton1 = Radiobutton(self, text="Nákup", variable=self.v1, value=1).pack(anchor=W)
-        self.radiobutton2 = Radiobutton(self, text="Prodej", variable=self.v1, value=2).pack(anchor=W)
-        self.v1.set(1)
-        
-        self.lbl1 = tk.Label(self, text="Měna:")
-        self.lbl1.pack(anchor=W)
-        self.radiobutton3 = Radiobutton(self, text="EUR", variable=self.v2, value=1).pack(anchor=W)
-        self.radiobutton4 = Radiobutton(self, text="GBP", variable=self.v2, value=2).pack(anchor=W)
-        self.radiobutton5 = Radiobutton(self, text="USD", variable=self.v2, value=3).pack(anchor=W)
-        self.radiobutton6 = Radiobutton(self, text="JPY", variable=self.v2, value=4).pack(anchor=W)
-        self.radiobutton7 = Radiobutton(self, text="IDR", variable=self.v2, value=5).pack(anchor=W)
-        self.v2.set(1)
-        
-        self.lbl1 = tk.Label(self, text="Kurz:")
-        self.lbl1.pack(anchor=W)
-        self.kurz=Text(self, height=1, width=16)
-        self.kurz.insert(tk.END, "100 CZK")
-        self.kurz.configure(state='disabled')
-        self.kurz.pack()
-        self.prevod=Text(self, height=1, width=16)
-        prevod100=100/kurz
-        self.prevod.insert(tk.END, prevod100)
-        self.prevod.configure(state="disabled")
-        self.prevod.pack()
-       
-        self.lbl1 = tk.Label(self, text="Výpočet:")
-        self.lbl1.pack(anchor=W)
-        self.listbox3 = Listbox(self,height=1)
-        self.listbox3.pack(anchor=W)
-        for item in [u""]:
-            self.listbox3.insert(END, item)
-        self.listbox4 = Listbox(self,height=1)
-        self.listbox4.pack(anchor=W)
-        for item in [u""]:
-            self.listbox4.insert(END, item)
-
-        self.bind("<Escape>", self.quit)
-        self.btn1 = tk.Button(self, text="Quit", command=self.quit)
-        self.btn1.pack()
-
-    def vypocet(self):  
-        pass
+okno = Tk()
+Label(okno, text=u"Směnárna").pack()
 
 
+transakceVar =  IntVar()
+transakceVar.set(0)
 
-    def listBoxFill(self):
-        f = open('listek.txt', 'r')
-        slovnik = {}
-        for line in f:
-            self.listBox.insert(tk.END,line.split()[0])
-            slovnik[line.split()[0]] = (line.split()[1:])
-        
+transakceRam = LabelFrame(okno, text="Transakce", padx=5, pady=5)
+transakceRam.pack(anchor=W)
 
-    def quit(self, event=None):
-        super().quit()
+Radiobutton(transakceRam,text="Nákup" , variable=transakceVar, value=0).grid(row=0,column=0)
+Radiobutton(transakceRam,text="Prodej", variable=transakceVar, value=1).grid(row=0,column=1)
 
 
-app = Application()
-app.mainloop()
+meny=kurzy()
+
+menaVar = IntVar()
+menaVar.set(0)
+menaRam = LabelFrame(okno, text="Měna", padx=5, pady=5)
+menaRam.pack(anchor=W)
+
+for n,item in enumerate(meny):
+    Radiobutton(menaRam,text=item[0], variable=menaVar, value=n).pack(anchor=W)
+
+kurzVar=StringVar()
+kurzVar.set('')
+mnozstviVar=StringVar()
+mnozstviVar.set('')
+
+kurzRam = LabelFrame(okno, text="Kurz", padx=5, pady=5)
+kurzRam.pack(anchor=W)
+mnozstviEdit = Entry(kurzRam, state='readonly', textvariable=mnozstviVar)
+mnozstviEdit.pack(anchor=W)
+kurzEdit = Entry(kurzRam, state='readonly', textvariable=kurzVar)
+kurzEdit.pack(anchor=W)
+
+vstupVar=StringVar()
+vstupVar.set('')
+vystupVar=StringVar()
+vystupVar.set('')
+
+vypocetRam = LabelFrame(okno, text="Výpočet", padx=5, pady=5)
+vypocetRam.pack(anchor=W)
+
+vstupEdit = Entry(vypocetRam, textvariable=vstupVar)
+vstupEdit.pack(anchor=W)
+
+vypocetButton= Button(vypocetRam,text="Výpočet",command=prepocet)
+vypocetButton.pack(anchor=E)
+
+vystupEdit = Entry(vypocetRam, state='readonly', textvariable=vystupVar)
+vystupEdit.pack(anchor=W)
+transakceVar.trace("w",prevody)
+menaVar.trace("w",prevody)
+okno.bind('<Map>', prevody)
+
+
+okno.mainloop()
